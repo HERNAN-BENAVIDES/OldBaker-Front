9ddaf881fe911c81fe43418697fad8bd71640b5c
HERNAN-BENAVIDES <hernand.benavidesc@uqvirtual.edu.co>
2025-09-02 00:35:35 -0500

    Agregar componentes: header y footer, y aplicar estilos globales

--- src/app/shared/header/header.ts
+++ src/app/shared/header/header.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  standalone: true,
+  imports: [RouterModule],
   templateUrl: './header.html',
-  styleUrl: './header.css'
+  styleUrls: ['./header.css']
 })
 export class Header {
 
