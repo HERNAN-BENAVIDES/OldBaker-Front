48ea8cdbb8d6770f274be5299d54249a64ddf5b4
HERNAN-BENAVIDES <hernand.benavidesc@uqvirtual.edu.co>
2025-09-06 20:54:44 -0500

    Actualizar URL de API para producción

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:8443' // URL para desarrollo
-  //apiUrl: 'https://api.mi-dominio.com' // URL real de producción
+  //apiUrl: 'https://localhost:8443' // URL para desarrollo
+  apiUrl: 'https://3.14.112.229' // URL real de producción
 
 };
